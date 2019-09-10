8751a30058c6c73d0c7c4e78616a7e9f8081d08b
raseemGit <raseem.moha@gmail.com>
2019-09-10 13:04:48 +0530

    For Json

--- src/test/java/org/stepdefinition/TestRunner.java
+++ src/test/java/org/stepdefinition/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = "org.stepdefinition", plugin = { "html:target",
+@CucumberOptions(features = "src/test/resources/Features", glue = "org.stepdefinition", plugin = { "json:target/cucumber.json",
 		"rerun:src/test/resources/Features/failedCases.txt" })
 
 public class TestRunner {
